59586606ffdaca5ff49d3045acff783b65631431
Swikarjadhav14 <123613073+Swikarjadhav14@users.noreply.github.com>
2023-06-04 21:14:35 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -21,26 +21,19 @@ def add_photo(frame, image_path, desired_size, position):
 
 def next():
     signin_frame1 = tk.Frame(root, bg="white", bd=6, relief="solid" ,highlightthickness=9, highlightbackground="pink")
-    signin_frame1.place(relx=0.5, rely=0.62, relwidth=0.4, relheight=0.7, anchor="center")  
-    
+    signin_frame1.place(relx=0.5, rely=0.62, relwidth=0.4, relheight=0.7, anchor="w")
     button1 = tk.Button(signin_frame1, text="Face Detection", font=("Helvetica", 20, "bold"), bg="white", fg="black", relief="solid", bd=2,padx=5,command=face_recog)
     button1.place(relx=0.08, rely=0.35)
-    
     button1.bind("<Enter>", on_enter)
     button1.bind("<Leave>", on_leave)
-    
     submit_button.bind("<Enter>", on_enter)
     submit_button.bind("<Leave>", on_leave)
-    
     button2 = tk.Button(signin_frame1, text="Fingerprint", font=("Helvetica", 20, "bold"), bg="white", fg="black", relief="solid", bd=2,padx=28)
     button2.place(relx=0.52, rely=0.35)
-    
     button2.bind("<Enter>", on_enter)
     button2.bind("<Leave>", on_leave)
-    
     tommy_button = tk.Label(signin_frame1, text="Click on any one mode of attendance",font=("Helvetica", 20, "bold"), bg="white", fg="black", padx=20, pady=10, relief="solid", bd=2, highlightthickness=1, highlightbackground="pink" )
     tommy_button.place(relx=0.04, rely=0.13)
-    
     add_photo(signin_frame1, "next1.jpg", (231, 270), (45, 280))
     add_photo(signin_frame1, "next2.jpg", (230, 270), (300, 280))
 
@@ -113,44 +106,38 @@ def face_recog():
     cv2.destroyAllWindows()   
 
 def open_link(event):
-    webbrowser.open("https://pict.edu")     
+    webbrowser.open("https://pict.edu")       
     
 background_label = tk.Label(root, bg='#29281f')
 background_label.place(x=0, y=0, relwidth=1, relheight=1)  
-
-title_label = tk.Label(root, text="Face Recognition Attendance System", font=("Helvetica", 32, "bold"), bg="#d68990", fg="black", padx=30, pady=30, relief="solid", bd=5, highlightthickness=5, highlightbackground="yellow")
+title_label = tk.Label(root, text="Face Recognition Attendance System", font=("Helvetica", 32, "bold"), bg="white", fg="black", padx=30, pady=30, relief="solid", bd=5, highlightthickness=5, highlightbackground="yellow")
 title_label.place(relx=0.5, rely=0.1, anchor="center")  
-
 current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 datetime_label = tk.Label(root, text=current_datetime, font=("Helvetica",21), bg="white", fg="black")
 datetime_label.place(relx=0.5, rely=0.215, anchor="center")  # Position the label below the title
+signin_frame = tk.Frame(root, bg="white", bd=6, relief="solid" ,highlightthickness=9, highlightbackground="white")
+signin_frame.place(relx=0.5, rely=0.62, relwidth=0.4, relheight=0.7, anchor="w") 
 
-signin_frame = tk.Frame(root, bg="#d6e3bc", bd=6, relief="solid" ,highlightthickness=9, highlightbackground="pink")
-signin_frame.place(relx=0.5, rely=0.62, relwidth=0.4, relheight=0.7, anchor="center") 
-
-signin_heading = tk.Label(signin_frame, text="Administrator Login", font=("Helvetica", 30, "italic"), bg="#89d6cd", fg="black", bd=2, relief="solid",highlightthickness=4, highlightbackground="grey")
+signin_heading = tk.Label(signin_frame, text="Administrator Login", font=("Helvetica", 30, "italic"), bg="white", fg="black", bd=2, relief="solid",highlightthickness=4, highlightbackground="grey")
 signin_heading.place(relx=0.17, rely=0.05)
-
 username_label = tk.Label(signin_frame, text="USERNAME", font=("Helvetica", 20), fg="black",bg="azure2",relief="solid",highlightthickness=3, highlightbackground="red")
 username_label.place(relx=0.09, rely=0.26)
-
 username_entry = tk.Entry(signin_frame, font=("Helvetica",20 ), fg="black", relief="solid", bd=2,bg="white")
 username_entry.place(relx=0.4, rely=0.265)
-
 password_label = tk.Label(signin_frame, text="PASSWORD", font=("Helvetica", 20),bg="azure2",  fg="black",relief="solid",highlightthickness=3, highlightbackground="red")
 password_label.place(relx=0.09, rely=0.42)
-
 password_entry = tk.Entry(signin_frame, show="*", font=("Helvetica", 20), fg="black", relief="solid", bd=2,bg="white")
 password_entry.place(relx=0.4, rely=0.425)
-
 submit_button = tk.Button(signin_frame, text="Submit", font=("Helvetica", 20, "bold"),  fg="black", relief="solid", bd=2,highlightthickness=3, highlightbackground="red",command=next)
 submit_button.place(relx=0.45,rely=0.6)
-
 submit_button.bind("<Enter>", on_enter)
 submit_button.bind("<Leave>", on_leave)
-
 link_label = ttk.Label(signin_frame, text="PICT PUNE", cursor="hand2", foreground="blue",font=("Helvetica", 20, "bold"),)
 link_label.pack(side='bottom')
 link_label.bind("<Button-1>", open_link)
+
+picture_frame = tk.Frame(root,bg="azure2", bd=6, relief="solid" ,highlightthickness=9, highlightbackground="white")
+picture_frame.place(relx=0.28, rely=0.62, relwidth=0.44, relheight=0.7, anchor="center") 
+add_photo(picture_frame, "How-Does-the-Attendance-Management-System-Help-the-Employees.jpg", (642, 570), (0, 0))
 
 root.mainloop()
